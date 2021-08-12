348b2d16003f0501c07aeaec9c54b1a22d3e2695
Vladyslav Verpeta <v.verpeta@beter.co>
2021-08-12 18:25:23 +0300

    initial

---
+++ src/TTClient.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace TestTaskSdkClient;
+
+use Exception;
+use http\Encoding\Stream;
+
+class TTClient
+{
+    private $payload;
+    private $endpoint;
+
+    public function __construct(string $endpoint)
+    {
+        $this->endpoint = $endpoint;
+    }
+
+    public function setPayload($payload)
+    {
+        $this->payload = $payload;
+
+        return $this;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function send()
+    {
+        if (empty($payload)) {
+            throw new Exception('Payload not set.');
+        }
+
+        return $this->internalSend();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function internalSend(): bool
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->endpoint);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "payload=" . urlencode(json_encode($this->payload)));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 90);
+        $server_output = curl_exec($ch);
+        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($responseCode !== 200) {
+            throw new Exception('Error while trying to send message.');
+        }
+        $this->payload = null;
+
+        return true;
+    }
+}
